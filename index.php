0278470bcace9f1481946e45cb5101c94d65b2db
RvanHulst <99051006@mydavinci.nl>
2020-03-31 14:51:25 +0200

    requires have been added in index

--- index.php
+++ index.php
@@ -1,19 +1,6 @@
 <<?php 
 
-$pikachu =  new Pokemon(
-	'Pikachu',
-	'Lightning', 
-	 60,
-	[['Electric Ring',50 ],['Pika Punch',20]],
-	['Fire', 1.5],
-	['Fighting',20]
-);
+require 'pokemon.php';
+require 'pikachu.php';
+require 'charmenleon.php';
 
-$charmeleon  =  new Pokemon(
-	'Charmeleon',
-	'Fire', 
-	 60,
-	[['Head Butt',10],['Flare',30]],
-	['Water', 2],
-	['Lightning',10]
-);
