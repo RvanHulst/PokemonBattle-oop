573358b308176cf1dcdfbbea89ec6b1b3464a3ce
RvanHulst <99051006@mydavinci.nl>
2020-03-31 10:26:06 +0200

    Pokemon classes gemaakt voor pika en char

---
+++ index.php
@@ -0,0 +1,19 @@
+<<?php 
+
+$pikachu =  new Pokemon(
+	'Pikachu',
+	'Lightning', 
+	 60,
+	[['Electric Ring',50 ],['Pika Punch',20]],
+	['Fire', 1.5],
+	['Fighting',20]
+);
+
+$charmeleon  =  new Pokemon(
+	'Charmeleon',
+	'Fire', 
+	 60,
+	[['Head Butt',10],['Flare',30]],
+	['Water', 2],
+	['Lightning',10]
+);
