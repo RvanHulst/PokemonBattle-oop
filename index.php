e5a6cc1518a2bce6120045b9ce37899046c37821
RvanHulst <99051006@mydavinci.nl>
2020-04-01 14:27:04 +0200

    gives message on the index

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
-<<?php 
-
+<?php 
 require 'pokemon.php';
 require 'pikachu.php';
-require 'charmenleon.php';
+require 'charmeleon.php';
 
+print_r("<pre>Charmeleon's health ". $charmeleon->health . "</pre>=Pikachu does a Electric Ring attack on Charmeleon=");
+$pikachu->attack($charmeleon, 0);
+print_r("<pre>Charmeleon's health after attack ". $charmeleon->health . '</pre>==Switching Turns==');
--- pokemon.php
+++ pokemon.php
@@ -8,7 +8,6 @@ class Pokemon {
     public $resistance =[]; 
     
     public function __construct($name, $energytype, $hitpoints, $attacks, $weakness, $resistance)
-
  {
         $health = $hitpoints;
         $this->name = $name;
@@ -17,4 +16,20 @@ class Pokemon {
         $this->attacks = $attacks;
         $this->weakness = $weakness;
         $this->resistance = $resistance;
+   }
+    public function attack($enemy, $attack){
+        $damage = $this->attacks[$attack][1];
+
+        if ($this->energytype == $enemy->weakness[0]) {
+           $damage = $damage * $enemy->weakness[1];
+        }
+        if ($this->energytype == $enemy->resistance[0]) {
+           $damage -= $enemy->resistance[1];
+        }
+        $enemy->health-=$damage;
     }
+ public function __toString() {
+      	return json_encode($this);
+    }
+
+}
