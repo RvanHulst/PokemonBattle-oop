2fb37431ff03044ebee2380198fd986599ff96fd
RvanHulst <99051006@mydavinci.nl>
2020-04-03 10:55:35 +0200

    attacks added in the index

--- index.php
+++ index.php
@@ -6,3 +6,6 @@ require 'charmeleon.php';
 print_r("<pre>Charmeleon's health ". $charmeleon->health . "</pre>=Pikachu does a Electric Ring attack on Charmeleon=");
 $pikachu->attack($charmeleon, 0);
 print_r("<pre>Charmeleon's health after attack ". $charmeleon->health . '</pre>==Switching Turns==');
+print_r("<pre>Pikachu's health ". $pikachu->health . "</pre>=Charmeleon does a Flare attack on Pikachu=");
+$charmeleon->attack($pikachu, 1);
+print_r("<pre>Pikachu's health after attack ". $pikachu->health . '</pre>');
