80ff30d2f2a67edcdf70acc7c54d58f4e6c324bb
Raaf <99051006@mydavinci.nl>
2020-06-03 11:38:51 +0200

    function attack($enemy, $attack readded

--- pokemon.php
+++ pokemon.php
@@ -17,4 +17,19 @@ class Pokemon {
         $this->weakness = $weakness;
         $this->resistance = $resistance;
    }
- 
+   public function attack($enemy, $attack){
+      $damage = $this->attacks[$attack][1];
+
+      if ($this->energytype == $enemy->weakness[0]) {
+         $damage = $damage * $enemy->weakness[1];
+      }
+      if ($this->energytype == $enemy->resistance[0]) {
+         $damage -= $enemy->resistance[1];
+      }
+      $enemy->health-=$damage;
+  }
+public function __toString() {
+       return json_encode($this);
+  }
+
+}
